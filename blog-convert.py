76d04c63aff2dbf9fa281ece0dcb4d1fb30557a5
Elan Ding <yirending@gmail.com>
2018-06-23 21:43:48 -0400

    updated

--- blog-convert.py
+++ blog-convert.py
@@ -15,16 +15,19 @@ read_css = open('custom/main_css.txt', 'r').read()
 read_gs = open('custom/main_js.txt','r').read()
 read_body = open(newFile, 'r').read()
 read_mathjax = open('custom/mathjax.txt', 'r').read()
+read_disqus = open('custom/disqus.txt', 'r').read()
+read_analytics = open('custom/analytics.txt', 'r').read()
 
 template = template.replace("[--navbar--]", "\n" + read_navbar + "\n")
 template = template.replace("[--js--]", "\n" + read_gs + "\n")
 template = template.replace("[--css--]", "\n" + read_css + "\n")
 template = template.replace("[--body--]", "\n" + read_body + "\n")
 template = template.replace("[--mathjax--]", "\n" + read_mathjax + "\n")
+template = template.replace("[--disqus--]", "\n" + read_disqus + "\n")
+template = template.replace("[--analytics--]", "\n" + read_analytics + "\n")
 
 with open(newFile, 'w') as f:
     f.write(template)
 
 Popen('cp ' + inFile + ' /Users/yirending/Documents/Github/yirending.github.io/blog/2018', shell=True).wait()
 Popen('mv ' + newFile + ' /Users/yirending/Documents/Github/yirending.github.io/blog/2018', shell=True).wait()
-
